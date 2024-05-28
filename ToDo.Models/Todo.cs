3e82dafac27beb08867518d6b33d2aa1d6288c1a
savasduzgun <savasdzgn@gmail.com>
2024-05-28 15:24:38 +0300

    Add project files.

---
+++ ToDo.Models/Todo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo.Models
+{
+    public class Todo : BaseModel
+    {
+        public string Description { get; set; }
+        public int StatusId { get; set; }
+
+        public virtual Status Status { get; set; }
+        public virtual ICollection<Tag> Tags { get; set; }
+    }
+}
---
+++ ToDo.Web/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using ToDo.Models;
+using ToDo.Web.Models;
+
+namespace ToDo.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            BaseModel baseModel = new BaseModel();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
