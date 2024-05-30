dfb384f3410335ec55d08cc53752143885c19d9a
savasduzgun <savasdzgn@gmail.com>
2024-05-30 06:39:33 +0300

    Etiket işlemleri için controller ve index view oluşturuldu

--- ToDo.Web/Controllers/HomeController.cs
+++ ToDo.Web/Controllers/HomeController.cs
@@ -9,16 +9,25 @@ namespace ToDo.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ApplicationDbContext _context;
-        public HomeController(ApplicationDbContext context)
+        public HomeController(ApplicationDbContext context)  //Dependency Injection ile ApplicationDbContext in nesnesini çekmiþ oluruz. new lemiyoruz çünkü program ilk çalýþtýðýnda otomatikman program.cs bizim için new                                                         liyor ve kullanýma hazýr olan objeyi dependencey injection ile controller a gönderiyor ve bu nesneyi bütün yazýlýmýn genelinde kullanabiliyoruz.
         {
             _context = context;
         }
 
         public IActionResult Index()
         {
+            //Tag tag = new Tag { Name = "Yazýlým" };
+            //_context.Tags.Add(tag);
 
-            var result = _context.Tags.ToList();
-            return View(result);
+            //List<Tag> tags = new List<Tag>();
+            //tags.Add(new Tag { Name = "Rutin" });
+            //tags.Add(new Tag { Name = "Ödev" });
+            //tags.Add(new Tag { Name = "Ev Ýþi" });
+
+            //_context.Tags.AddRange(tags);  //Birden fazla veri eklemek için
+            //_context.SaveChanges();
+
+            return View();
         }
 
     }
---
+++ ToDo.Web/Controllers/TagController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDo.Data;
+
+namespace ToDo.Web.Controllers
+{
+    public class TagController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TagController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Tags.ToList());
+        }
+    }
+}
