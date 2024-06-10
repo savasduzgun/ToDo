81e77f11f6e8b9e01be8864bc2e090d8c6e794e5
savasduzgun <savasdzgn@gmail.com>
2024-06-10 14:14:29 +0300

    Status controller eklendi

---
+++ ToDo.Web/Controllers/StatusController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDo.Web.Controllers
+{
+    public class StatusController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
