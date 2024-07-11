131d1ec94823bc130d18356b592b72f4bfe71874
savasduzgun <savasdzgn@gmail.com>
2024-07-11 14:07:04 +0300

    ToDo controller eklendi

---
+++ ToDo.Web/Controllers/ToDoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDo.Web.Controllers
+{
+    public class ToDoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
