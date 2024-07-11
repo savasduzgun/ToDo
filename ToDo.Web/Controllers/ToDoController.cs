35ff4a051617e3a9e1054fe329323a641240ec9d
savasduzgun <savasdzgn@gmail.com>
2024-07-11 14:08:54 +0300

    Dependency Injection ile AplicationDbContext in referansı yakalandı

--- ToDo.Web/Controllers/ToDoController.cs
+++ ToDo.Web/Controllers/ToDoController.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ToDo.Data;
 
 namespace ToDo.Web.Controllers
 {
     public class ToDoController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public ToDoController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
